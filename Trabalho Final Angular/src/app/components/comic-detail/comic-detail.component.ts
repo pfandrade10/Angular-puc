d1d15d9bf6cd48576d708222bf52dd1def8eee48
Pedro Andrade <pfandrade10@hotmail.com>
2021-06-13 21:49:54 -0300

    colocando rotas de navegação, e arrumando tela

--- Trabalho Final Angular/src/app/components/comic-detail/comic-detail.component.ts
+++ Trabalho Final Angular/src/app/components/comic-detail/comic-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlertService, AlertType } from '../../services/alert.service';
+import { MarvelService } from '../../services/marvel.service';
 
 @Component({
   selector: 'app-comic-detail',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComicDetailComponent implements OnInit {
 
-  constructor() { }
+  public comicDetail: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private marvelService: MarvelService,
+    private alertService: AlertService,
+  ) {}
 
   ngOnInit(): void {
+    this.populateComic()
   }
+
+  populateComic(){
+    this.route.params.subscribe((params) => {
+      this.marvelService.getComic(params.id).subscribe(response => {
+        this.comicDetail = response.data.results[0];
+        console.log(this.comicDetail)
+      }),
+      (error)=>{
+        console.log(error)   
+        this.alertService.show('Erro inesperado', error, AlertType.error);
+      };
+    });  
+  } 
 
 }
